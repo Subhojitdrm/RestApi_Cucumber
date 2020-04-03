f68b88fe0fcf81e7cc64fb8c0d207b9d44bb72db
Subhojitdrm <55389744+Subhojitdrm@users.noreply.github.com>
2020-04-04 02:14:11 +0530

    Tada

--- src/test/java/Test_o1/Test_Get_Method_01.java
+++ src/test/java/Test_o1/Test_Get_Method_01.java
@@ -1,13 +1,34 @@
 package Test_o1;
 
-import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+import org.hamcrest.core.Is;
 import org.testng.annotations.Test;
 
 public class Test_Get_Method_01 {
 	
 	@Test
 	public void thisIs() {
-		System.out.println("Try this");
+		given()
+		.when()
+			.get("http://dummy.restapiexample.com/api/v1/employees")
+		.then()
+			.assertThat().statusCode(200);
+	
 	}
+	@Test
+	public void validateReport() {
+		
+		given()
+		.when()
+		.get("http://dummy.restapiexample.com/api/v1/employees")
+		.then()
+		.assertThat()
+		.body("status", Is.is("success"));
+		
+	}
+	
+	
+	
 	
 }
