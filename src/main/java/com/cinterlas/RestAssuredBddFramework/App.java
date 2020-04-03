8ef4882e11a8ebe79b022869df8d45b15c52d97f
Subhojitdrm <55389744+Subhojitdrm@users.noreply.github.com>
2020-04-04 00:53:07 +0530

    this is step 01

---
+++ src/main/java/com/cinterlas/RestAssuredBddFramework/App.java
@@ -0,0 +1,13 @@
+package com.cinterlas.RestAssuredBddFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
